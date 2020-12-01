ffdcb1f4670ca022055d5453cf1562329c228a0f
Jasper Edbrooke <jasperredrock@gmail.com>
2020-11-30 23:16:21 -0800

    replace named constants with their actual values

--- src/EX/ALU_tb.v
+++ src/EX/ALU_tb.v
@@ -27,29 +27,29 @@ module ALU_tb (
     parameter WIDTH = 32;
     reg [WIDTH-1:0] a = 0;
     reg [WIDTH-1:0] b = 0;
-    reg [3:0] control = `ALU_ADD;
+    reg [3:0] control = 4'b0;
     reg clk = 0;
     wire [WIDTH-1:0] out;
     wire zero;
     wire overflow;
-    integer i = 0
+    integer i = 0;
 
 
     reg [15:0] operation [3:0];
-    operation[`ALU_ADD] = `ALU_ADD ;
-    operation[`ALU_SUB] = `ALU_SUB ;
-    operation[`ALU_SLL] = `ALU_SLL ;
-    operation[`ALU_XOR] = `ALU_XOR ;
-    operation[`ALU_SRL] = `ALU_SRL ;
-    operation[`ALU_SRA] = `ALU_SRA ;
-    operation[`ALU_OR ] = `ALU_OR  ;
-    operation[`ALU_AND] = `ALU_AND ;
-    operation[`ALU_SLT] = `ALU_SLT ;
-    operation[`ALU_SGE] = `ALU_SGE ;
-    operation[`ALU_SLT] = `ALU_SLTU;
-    operation[`ALU_SGE] = `ALU_SGEU;
-    operation[`ALU_JMP] = `ALU_JMP ;
-    operation[`ALU_ERR] = `ALU_ERR ;
+    operation[4'b0000] = 4'b0000; // ALU_ADD 
+    operation[4'b1000] = 4'b1000; // ALU_SUB 
+    operation[4'b0001] = 4'b0001; // ALU_SLL 
+    operation[4'b0100] = 4'b0100; // ALU_XOR 
+    operation[4'b0101] = 4'b0101; // ALU_SRL 
+    operation[4'b1101] = 4'b1101; // ALU_SRA 
+    operation[4'b0110] = 4'b0110; // ALU_OR  
+    operation[4'b0111] = 4'b0111; // ALU_AND 
+    operation[4'b0010] = 4'b0010; // ALU_SLT 
+    operation[4'b1010] = 4'b1010; // ALU_SGE 
+    operation[4'b0011] = 4'b0011; // ALU_SLTU
+    operation[4'b1011] = 4'b1011; // ALU_SGEU
+    operation[4'b1110] = 4'b1110; // ALU_JMP 
+    operation[4'b1111] = 4'b1111; // ALU_ERR 
 
 
     ALU #(.WIDTH(WIDTH)) uut (
@@ -63,6 +63,20 @@ module ALU_tb (
     );
 
     initial begin
+        operation[4'b0000] = 4'b0000;
+        operation[4'b1000] = 4'b1000;
+        operation[4'b0001] = 4'b0001;
+        operation[4'b0100] = 4'b0100;
+        operation[4'b0101] = 4'b0101;
+        operation[4'b1101] = 4'b1101;
+        operation[4'b0110] = 4'b0110;
+        operation[4'b0111] = 4'b0111;
+        operation[4'b0010] = 4'b0010;
+        operation[4'b1010] = 4'b1010;
+        operation[4'b0011] = 4'b0011;
+        operation[4'b1011] = 4'b1011;
+        operation[4'b1110] = 4'b1110;
+        operation[4'b1111] = 4'b1111;
         #10;
         #10 a = 10;
         b = 10;
--- src/parameters.vh
+++ src/parameters.vh
@@ -79,14 +79,14 @@
 `define ALUOp_jmp {2'b11}
 
 //ALU CONTROLS
-`define ALU_ADD {1'b0000}
-`define ALU_SUB {1'b1000}
-`define ALU_SLL {1'b0001}
-`define ALU_XOR {1'b0100}
-`define ALU_SRL {1'b0101}
-`define ALU_SRA {1'b1101}
-`define ALU_OR  {1'b0110}
-`define ALU_AND {1'b0111}
+`define ALU_ADD {4'b0000}
+`define ALU_SUB {4'b1000}
+`define ALU_SLL {4'b0001}
+`define ALU_XOR {4'b0100}
+`define ALU_SRL {4'b0101}
+`define ALU_SRA {4'b1101}
+`define ALU_OR  {4'b0110}
+`define ALU_AND {4'b0111}
 `define ALU_SLT {4'b0010}
 `define ALU_SGE {4'b1010}
 `define ALU_SLTU{4'b0011}
