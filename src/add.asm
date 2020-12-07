8f0b068f59acf3ee4050ecf5f8a384148cfc3a6f
Jasper Edbrooke <jasperedbrooke@gmail.com>
2020-12-07 14:26:37 -0800

    add add.asm

---
+++ src/add.asm
@@ -0,0 +1,18 @@
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+addi x1 x0 1
+jal x0 EXIT
+EXIT:
